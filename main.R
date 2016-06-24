f6eb7142447ad7b660612d5f02bc6f7661bb8a71
alephO <jlch@brandeis.edu>
2016-06-24 16:24:31 -0400

    Update

--- main.R
+++ main.R
@@ -7,9 +7,18 @@ origin_data <- read.csv("sp500_industrygroup.csv")
 names(origin_data) <- sub("[ .]*index$",'',names(origin_data),ignore.case = TRUE)
 
 ## Assign Useful tages
-date <- "DATE"
+date <- "Date"
 market <- "MRKT"
 prices <- names(origin_data)[3:26]
 
 
-## Process with data
+## Calculate Increase Rate
+if(!exists("cal_increase_rate", mode="function")) source("cal-rate.R")
+ic_data<-cal_increase_rate(origin_data,date,market,prices)
+
+## Remove rows with ALL data missing
+ic_data <- ic_data[rowSums(is.na(ic_data[,c(market,prices)]))<length(c(market,prices)),]
+
+
+## Calculate mrk_rel
+mr_data <- cal_mrk_rel(ic_data,market,prices)
